7e940ef36bdaa527af0ee42588431bec57ec6ac3
Rakshit Nawani <nawanirakshit@gmail.com>
2024-04-03 12:49:19 +0530

    Initial commit

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,11 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
